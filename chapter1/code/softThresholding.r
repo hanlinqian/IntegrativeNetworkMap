79aa7a1bbd6ef9faab25072e174173ee4d111403
Linqian Han <hanlinqian@qq.com>
2022-05-02 18:57:00 +0800

    Add files via upload

---
+++ chapter1/code/softThresholding.r
@@ -0,0 +1,15 @@
+library(WGCNA)
+args<-commandArgs(T)
+powers1=c(seq(1,10,by=1),seq(12,30,by=2))
+datExpr=read.table(args[1],sep="\t",row.names=1,header=T,check.names=F)
+datExpr = t(datExpr)
+RpowerTable=pickSoftThreshold(datExpr, powerVector=powers1)[[2]]
+cex1=0.9
+pdf(file="softThresholding.pdf")
+par(mfrow=c(1,2))
+plot(RpowerTable[,1], -sign(RpowerTable[,3])*RpowerTable[,2],xlab="Soft Threshold (power)",ylab="Scale Free Topology Model Fit,signed R^2",type="n")
+text(RpowerTable[,1], -sign(RpowerTable[,3])*RpowerTable[,2], labels=powers1,cex=cex1,col="red")
+abline(h=0.85,col="red")
+plot(RpowerTable[,1], RpowerTable[,5],xlab="Soft Threshold (power)",ylab="Mean Connectivity", type="n")
+text(RpowerTable[,1], RpowerTable[,5], labels=powers1, cex=cex1,col="red")
+dev.off()
