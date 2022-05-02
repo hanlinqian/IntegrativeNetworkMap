1fcbeb0abed7b18cfed3da65fd2e7a9351bd363b
Linqian Han <hanlinqian@qq.com>
2022-05-02 20:54:36 +0800

    Delete totalrna-rep2.r

--- chapter1/code/totalrna-rep2.r
+++
@@ -1,6 +0,0 @@
-library(WGCNA)
-datExpr=read.table("totalrna-log-rep2.txt",sep="\t",row.names=1,header=T,check.names=F)
-datExpr = t(datExpr)
-beta1=12
-ADJ= adjacency(datExpr,power=beta1)
-vis=exportNetworkToCytoscape(ADJ,edgeFile="totalrna-edge-rep2.txt",threshold = 0.1)
