24b19a2435d73ba67a38070d182bb7a6f4502b03
Linqian Han <hanlinqian@qq.com>
2022-05-03 20:16:30 +0800

    Update totalrna-rep1.r

--- chapter1/code/totalrna-rep1.r
+++ chapter1/code/totalrna-rep1.r
@@ -2,6 +2,6 @@ library(WGCNA)
 args<-commandArgs(T)
 datExpr=read.table(args[1],sep="\t",row.names=1,header=T,check.names=F)
 datExpr = t(datExpr)
-beta1=18
+beta1=args[2]
 ADJ= adjacency(datExpr,power=beta1)
-vis=exportNetworkToCytoscape(ADJ,edgeFile=args[2],threshold = 0.1)
+vis=exportNetworkToCytoscape(ADJ,edgeFile=args[3],threshold = 0.1)
